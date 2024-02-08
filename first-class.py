fa008ecf1aac605c0d3f608a0100f024ac56637e
Saurabh Aryal <saurabharyal10@gmail.com>
2024-02-08 09:26:04 +0545

    Initial Commit

---
+++ first-class.py
@@ -0,0 +1,74 @@
+from tkinter import Y
+
+
+print("hello")
+#it is used for comment
+
+# variable  -> variable is used to store value
+# x = 12, here = means assignment operator
+#text = "hello world 2022"
+#when we declare variable we cannot use inbuilt methods/function and cannot use keywords
+
+x= 20
+print(x)
+
+text = "python with django"
+print(text)
+
+#assignment operator
+# = -> value assign
+# x+=y -> x = x+y
+# x-=y -> x = x-y 
+# x%=y -> x = x%y
+# x*=y -> x = x*y
+# x/=y -> x = x/y   
+# x//=y -> x = x//y     [// floor division(wont count after decimal)] -> 5//2 =2
+
+
+x=5
+y=10
+x+=y
+print(x)
+
+x-=y
+print(x)
+
+a=5
+b=2
+a//=2
+print(a)
+
+#arithmetic operator
+# + (addition)
+# - (subtraction)
+# * (multiplication)
+# / (division)
+# // (floor division)
+# ** (power)
+
+#logical operator
+# or
+# and
+# not
+
+#comparison operator
+# ==(equal to)
+# === (equal value and equal data type)
+# <,>,<=,>=  etc
+
+#data type
+# int (integer)
+# float (floating number i.e 12.34)
+#str (String)
+#list (in other programming it called as an array)
+#tuple
+#dict(dictionery) -> it is called as an object other programming
+#set
+#boolean
+
+#for determining data type use type()
+d = 5.5 
+print(type(d))
+
+e= "Welcome "
+print(type(e))
