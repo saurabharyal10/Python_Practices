ceae90374f34269c1e50128f377dd46254ff9c8c
Saurabh Aryal <saurabharyal10@gmail.com>
2024-02-08 09:27:16 +0545

    Initial Commit

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#dictionary are always comes in key value pair
+# in other language it is knowns as object
+
+student = {
+    'first_name':'Saurabh',
+    'last_name':'Aryal',
+    'age':21,
+    'height':5.8,
+    'gender':'Male'
+} 
+
+print(student['first_name'])
+print(student['height'])
