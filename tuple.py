496411ab03ee8f598c1f322639bc46a580debae4
Saurabh Aryal <saurabharyal10@gmail.com>
2024-02-10 07:39:18 +0545

    Initial Commit

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#tuple similar to list but immutable ie value inside tuple cannot be change
+
+web = ('html','css','javascript','bootstrap','react')
+print(web)
+
+# use len() to count the length of tuple
+print(len(web))
+
+
